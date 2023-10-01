d26e88aefab7b30249cc705bc965b3db4e433bde
JPraskala <jacob.praskala@astroaitrading.com>
2023-10-01 13:39:44 -0400

    Update

--- Assets/Testing/Test Scripts/TestEnemy.cs
+++ Assets/Testing/Test Scripts/TestEnemy.cs
@@ -2,5 +2,27 @@ using UnityEngine;
 
 public class TestEnemy : MonoBehaviour
 {
-    
+    GameObject player;
+    Rigidbody2D rb;
+    CircleCollider2D circle;
+
+    void Start() 
+    {
+        if (!TryGetComponent<Rigidbody2D>(out rb) || !TryGetComponent<CircleCollider2D>(out circle)) 
+        {
+            print("Components were not found.");
+            Application.Quit(1);
+        }
+
+        player = GameObject.Find("Test_Player");
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.name == "Test_Player") 
+        {
+            gameObject.SetActive(false);
+            print("Destroyed");
+        }
+    }
 }
